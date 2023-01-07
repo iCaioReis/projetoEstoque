400bba0ba76587b8d4225cf203df52ca5a88ef4e
icaioreis <caioreis3186@gmail.com>
2023-01-07 00:48:35 -0300

    VERIFICANDO USUÁRIO LOGADO NAS ROTAS

--- src/routes/usuario.route.js
+++ src/routes/usuario.route.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controllers/usuario.controller');
+const verifyJWT = require('../middlewares/autorizator');
 const usuarioValidator = require ('../validators/usuario.validator')
 
 router.post('/', usuarioValidator.criar(), usuarioController.create);
 
 router.post('/login', usuarioValidator.login(), usuarioController.login);
 
-router.get('/', usuarioController.findAll);
+router.get('/', verifyJWT, usuarioController.findAll);
 
-router.get('/:id', usuarioValidator.findById(), usuarioController.findById);
+router.get('/:id', verifyJWT, usuarioValidator.findById(), usuarioController.findById);
 
-router.put('/:id', usuarioValidator.atualizar(), usuarioController.atualizar);
+router.put('/:id', verifyJWT, usuarioValidator.atualizar(), usuarioController.atualizar);
 
-router.delete('/:id', usuarioValidator.deleteById(), usuarioController.deleteById);
+router.delete('/:id', verifyJWT, usuarioValidator.deleteById(), usuarioController.deleteById);
 
 module.exports =  router;
